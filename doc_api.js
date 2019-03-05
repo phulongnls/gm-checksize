e5276dfddda3aee3e22393cb6b56aef185af94ad
phulongnls <32897534+phulongnls@users.noreply.github.com>
2019-03-05 14:10:53 +0700

    Update doc_api.js

--- doc_api.js
+++ doc_api.js
@@ -1,52 +1,15 @@
-/**
-
- * @api {get} /resize/?width=:width&height=:height  request resize image
- * @apiName Resize
- *
- * @apiParam {Number} [width] width attribute of the IMG resized.
- * @apiParam {Number} [height] height attribute of the IMG resized.
- *
-
- * @apiSuccessExample {json} [Success-Response]:
- *     HTTP/1.1 200 OK
- *     {
- *      "code":200,
- *      "messsage":"resize success"
- *     }
- *
- * @apiError [error] Can not resize image
- * @apiError [code] 400
- *
- * @apiErrorExample {json} [Error-Response]:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "code":"400",
- *       "error":"resize not success"
- *     }
- 
- * @api {post} /check_size/  request check information image from URL
- * @apiName CheckSize
- *
- * @apiParam {String} image_url url of Image.
- *
- * @apiSuccess {String} [width] Width of the Image.
- * @apiSuccess {String} [height]  Height of the Image.
- *
- * @apiSuccessExample {json} [Success-Response]:
- *     HTTP/1.1 200 OK
- *     {
- *       "width": "272",
- *       "height": "92"
- *     }
- *
- * @apiError [error] ImageNotFound
- * @apiError [code] 400
- *
- * @apiErrorExample {json} [Error-Response]:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "ImageNotFound",
- *       "code": "400"
- *     }
-
-*/
+/** **  REQUIRED API DOCS TO BE COMPATIBLE WITH KINTOHUB **
+ * @api {POST} /vehicle/{type} Add a new vehicle to the logged in user's collection
+ * @apiName AddVehicle
+ * @apiHeader (Session) {String} auth-account-id Logged in User's Id
+ * @apiHeader {String} authorization Logged in User's Id
+ * @apiParam (Url) {String} type the vehicle type
+ * @apiParam {String} model the vehicle model
+ * @apiParam {String} name the vehicle name
+ ** OPTIONAL API DOCS **
+ * @apiSuccess (Session) {Object[]} vehicle-owned All the vehicles that the current user have
+ * @apiSuccess {Object} data the vehicle object
+ * @apiSuccess {String} data.id Unique id for the vehicle
+ * @apiSuccess {String} data.type vehicle type
+ * @apiSuccess {String} data.model vehicle model
+ */
